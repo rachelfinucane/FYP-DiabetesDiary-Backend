aecf60fc90553123fbb24a177fd8d2f8b7bd2b91
rachelfinucane <55754303+rachelfinucane@users.noreply.github.com>
2022-08-09 20:39:16 +0100

    added academic disclaimer to footer started adding logs page

--- routes/logs.js
+++ routes/logs.js
@@ -14,5 +14,14 @@ router.get('/add-logs', function(req, res, next) {
   res.render('add-logs', { user: req.user });
 });
 
+/* GET logs page. */
+router.get('/logs', function(req, res, next) {
+  if (!req.user) { return res.render('home'); }
+  next();
+}, function(req, res, next) {
+  res.locals.filter = null;
+  res.render('logs', { user: req.user });
+});
+
 
 module.exports = router;
