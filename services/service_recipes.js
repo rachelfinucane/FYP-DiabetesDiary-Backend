4efd6e841ff0536d2bc9360f6386536c9490a364
rachelfinucane <55754303+rachelfinucane@users.noreply.github.com>
2022-08-18 18:40:18 +0100

    removed some unneeded commented code

--- services/service_recipes.js
+++ services/service_recipes.js
@@ -4,19 +4,6 @@ const { parse } = require('node-html-parser')
 
 async function scrapeNutritionInfo(service, url) {
 
-    // let response = await axios.get(url);
-
-    // const root = parse(response.data.toString());
-
-    // const infoNode = root.getElementById('__NEXT_DATA__');
-    // const infoContent = (JSON.parse(infoNode.innerHTML)).props.pageProps;
-    // return {
-    //     title: infoContent.title,
-    //     description: infoContent.description,
-    //     ingredients: infoContent.ingredients,
-    //     nutritionalInfo: infoContent.nutritionalInfo,
-    //     servings: infoContent.servings
-    // }
     if(service === 'BBC'){
         return scrapeBBC(url);
     }
